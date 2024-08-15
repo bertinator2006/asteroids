e2b79befa0ad12f3e1141d364e8e2b659755d668
bertinator2006 <poudelaayush2006@gmail.com>
2024-08-15 23:20:53 +1000

    Removed unnessacary files.

--- VSCode/main.c
+++ VSCode/main.c
@@ -1,19 +1,41 @@
 #include "raylib.h"
+#include <math.h>
 
-static void UpdateDrawFrame(void);
+#define GAME_SPEED 10
+
+struct PlayerStruct
+{
+    Vector2 position;
+    Vector2 velocity;
+    float angle_rad;
+    float radius;
+    Color color;
+};
+
+static void UpdateDrawFrame(struct PlayerStruct *player);
+void DrawPlayer(Vector2 position, float angle, float radius, Color color);
+
 
 int main()
 {
     const int screenWidth = 800;
     const int screenHeight = 450;
 
+    struct PlayerStruct player = {
+        .position = (Vector2){screenWidth / 2, screenHeight / 2},
+        .velocity = (Vector2){0, 0},
+        .angle_rad = 0,
+        .radius = 10,
+        .color = RED
+    };
+
     InitWindow(screenWidth, screenHeight, "raylib");
 
-    SetTargetFPS(60);
+    SetTargetFPS(144);
 
     while (!WindowShouldClose())
     {
-        UpdateDrawFrame();
+        UpdateDrawFrame(&player);
     }
 
     CloseWindow();
@@ -22,15 +44,35 @@ int main()
 
 
 
-static void UpdateDrawFrame(void)
-{   
+static void UpdateDrawFrame(struct PlayerStruct *player)
+{
+    float delta_time = GetFrameTime();
+    if (IsKeyDown(KEY_RIGHT)) player->angle_rad += 0.5 * GAME_SPEED * delta_time;
+    if (IsKeyDown(KEY_LEFT)) player->angle_rad -= 0.5 * GAME_SPEED * delta_time;
+    //TODO: Use vectors to move the player ore towards the direction they are facing
+
+    
     BeginDrawing();
 
-        ClearBackground(RAYWHITE);
-
+        ClearBackground(BLACK);
         DrawText("This is a raylib example", 10, 40, 20, DARKGRAY);
-
+        DrawPlayer(player->position, player->angle_rad, player->radius, player->color); 
         DrawFPS(10, 10);
 
     EndDrawing();
 }
+
+//finished
+void DrawPlayer(Vector2 position, float angle, float radius, Color color)
+{
+    Vector2 p1, p2, p3;
+    p1 = (Vector2){position.x + cos(angle) * radius, position.y + sin(angle) * radius};
+    p2 = (Vector2){position.x + cos(angle + 2.356) * radius, position.y + sin(angle + 2.356) * radius};
+    p3 = (Vector2){position.x + cos(angle - 2.356) * radius, position.y + sin(angle - 2.356) * radius};
+    DrawLineV(p1, p2, color);
+    DrawLineV(p1, p3, color);
+    DrawLineV(p2, position, color);
+    DrawLineV(p3, position, color);
+}
+
+
